85ea082e71c6ae16906f0230a5c58fd523f963be
DillonHale <Drh9473@g.rit.edu>
2014-04-06 00:08:25 -0400

    Updated Server

--- server.pl
+++ server.pl
@@ -4,3 +4,14 @@ system "nc -l -p 3000 >> testFile | tee output.log";
 
      print "Checksum == ";
      system "md5sum memoryImage.dd";
+	 
+print "Select an Option to perform Analysis on the acquired memory image\n";
+print " 1) View Strings\n\n";
+
+print "Choice > ";
+$choice = <>;
+
+if ($choice == 1)
+{
+    system ("strings memoryImage.dd | more");
+}
