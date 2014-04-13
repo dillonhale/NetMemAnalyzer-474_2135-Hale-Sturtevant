786341b4f6d5661ffeefc1199720faeaae418f68
aas5999 <aas5999@rit.edu>
2014-04-12 22:50:10 -0400

    Update server.pl
    Uses perl Tk module to create the GUI

--- server.pl
+++ server.pl
@@ -1,6 +1,35 @@
 #/usr/bin/perl
 
+use strict;
+use Tk;
+
 system "nc -l -p 3000 >> testFile | tee output.log";
 
-     print "Checksum == ";
-     system "md5sum memoryImage.dd";
+print "Checksum == ";
+system "md5sum memoryImage.dd";
+
+my $mw = MainWindow->new;
+$mw->geometry("300x300");
+$mw->title("NetMemAnalyzer");
+
+$mw->Label( -text => "What would you like to analyze?" )
+   ->grid(
+	-row => 2,
+	-column => 1,
+	-columnspan =>2);
+       
+$mw->Button(
+     -text => "View Strings",
+     -command => sub{
+
+			system ("strings memoryImage.dd | more");
+		
+		      })
+
+      ->grid(
+	-row => 3,
+	-column => 1,
+	-columnspan => 2);
+
+
+MainLoop;
