c651f79b0d81dd983a7b3710762a649d984df281
dillonhale <Drh9473@g.rit.edu>
2014-04-30 19:12:45 -0400

    Update to Client
    Removed GUI on client so it is less resource intensive.

--- client.pl
+++ client.pl
@@ -1,111 +1,24 @@
 #!/usr/bin/perl -w
 
 use strict;
-use Tk;
 
-my $countVar;
+my $blockSize;
 my $serverIP;
 my $serverPort;
 
+print "Enter Block Size: ";
+$blockSize = <>;
 
-my $mw = MainWindow->new;
-$mw->geometry("300x300");
-$mw->title("NetMemAnalyzer");
+print "Enter Server IP: ";
+$serverIP = <>;
 
+print "Enter Server Port: ";
+$serverPort = <>;
 
+chomp($blockSize);
+chomp($serverIP);
+chomp($serverPort);
 
-#creates a label called "Enter blocksize" located in row2, column1, with a width of one columnspan in the window
-$mw->Label( -text => "Enter blocksize" )
-   ->grid(
-	-row => 2,
-	-column => 1,
-	-columnspan =>1);
+my $transferFile = `dd if=/dev/fmem bs=$blockSize of=memoryImage.dd`;
 
-$mw->Label( -text => "Enter Server IP" )
-   ->grid(
-	-row => 3,
-	-column => 1,
-	-columnspan =>1);
-
-$mw->Label( -text => "Enter Server Port" )
-   ->grid(
-	-row => 4,
-	-column => 1,
-	-columnspan =>1);
-
-
-my $blocksize_entered = $mw->Entry( -width => 10);
-$blocksize_entered
-    ->grid(
-	-row =>2,
-	-column => 2,
-	-columnspan =>1);
-
-
-my $ip_entered = $mw->Entry( -width => 10);
-$ip_entered
-    ->grid(
-	-row =>3,
-	-column => 2,
-	-columnspan =>1);
-
-my $port_entered = $mw->Entry( -width => 10);
-$port_entered
-     ->grid(
-	  -row =>4,
-	  -column => 2,
-	  -columnspan => 1);  	
-
-$mw->Button(
-     -text => "Connect to server",
-     -command => sub{
-			#countVar = 4096
-			#serverIP = 127.0.0.1
-			 $countVar = $blocksize_entered->get; 
-			 $serverIP = $ip_entered->get;
-			 $serverPort = $port_entered;	
-			
-			
-			#extra
-			$mw->Label( -text => "Please Wait.." )
-			   ->grid(
-				-row => 6,
-				-column => 1,
-				-columnspan =>1);
-
-			my $count =1;
-sub connected{
-
-	$mw->destroy;
-		my $new = MainWindow->new;
-		$new->geometry("300x300");
-		$new->title("NetMemAnalyzer");
-	$new->Label( -text => "Successfully connected to server" )
-   ->grid(
-	-row => 2,
-	-column => 1,
-	-columnspan =>2);
-
-
-my $transferFile = `dd if=/dev/fmem count=4096 of=memoryImage.dd`;
-
-    system "nc $serverIP $serverPort < memoryImage | tee output.log";
-   print "Checksum == ";
-  system "md5sum memoryImage.dd";
-
-}
-print $count;
-if($count == 1)
-{
-connected();
-}
-			
-		      })
-
-      ->grid(
-	-row => 5,
-	-column => 1,
-	-columnspan => 2);
-
-
-MainLoop;
+system "nc $serverIP $serverPort < memoryImage.dd";
